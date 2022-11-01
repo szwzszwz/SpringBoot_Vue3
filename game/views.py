0602cb1b7142eba471583b83b66d1243342594b0
zhangwenzhe song <1422297350@qq.com>
2022-11-01 22:06:58 +0800

    write my first page

--- game/views.py
+++ game/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    line1 = '<h1 style="text-align: center">first htmls!</h1>'
+    line2 = '<a href="/play/">enter game</a>'
+    return HttpResponse(line1 + line2)
+
+def play(request):    
+    line1 = '<h1 style="text-align: center">game htmls!</h1>'
+    line2 = '<a href="/">return game</a>'
+    return HttpResponse(line1 + line2)
+    
